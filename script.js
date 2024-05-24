e291c7a3b188842b3c2e999c7fc8e6c5d1286fed
Edgar Alcina <160753417+Edzin32@users.noreply.github.com>
2024-05-24 03:34:03 +0000

    correção

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
-function sprite(contexto,imagem,x,y,deslocamento){
-this.contexto=contexto;
 
-this.imagem=imagem;
-this.x=x;
-this.y=y;
+ canvas = document.getElementById("canv");
+     ctx = canvas.getContext("2d");
 
-this.deslocamento=deslocamento;
-this.largura=this.imagem.width;
-this.altura=this.imagem.height;
+     imgNave = new Image();
+    imgNave.src = "image.png";
+    imgNave.onload = criaNave;
 
-this.desenhar=function(){
-    this.contexto.drawimage(this.x,this.y,this.largura,this.altura);
+    function Sprite(contexto, imagem, x, y, deslocamento) {
+        this.contexto = contexto;
+        this.imagem = imagem;
+        this.x = x;
+        this.y = y;
+        this.deslocamento = deslocamento;
+        this.largura = this.imagem.width;
+        this.altura = this.imagem.height;
 
-}
-this.direita=function(){
-    if(this.x<this.contexto.canvas.width-this.largura-this.deslocamento){
-        this.x+=this.deslocamento;
+        this.desenhar = function() {
+            this.contexto.drawImage(this.imagem, this.x, this.y-177, this.largura-80, this.altura-80);
+        };
+
+        this.direita = function() {
+            if (this.x < this.contexto.canvas.width - this.largura - this.deslocamento) {
+                this.x += this.deslocamento;
+            }
+        };
+
+        this.esquerda = function() {
+            if (this.x > 0) {
+                this.x -= this.deslocamento;
+            }
+        };
+
+        this.subir = function() {
+            if (this.y-180 > 0) {
+                this.y -= this.deslocamento;
+            }
+        };
+
+        this.descer = function() {
+            if (this.y-177 <= this.contexto.canvas.height - this.altura - this.deslocamento) {
+                this.y += this.deslocamento;
+            }
+        };
     }
-}
-this.esquerda=function(){
-    if(this.x>0){
-        this.x-=this.deslocamento;
+
+    function criaNave() {
+         nave = new Sprite(ctx, imgNave, canvas.width / 2 - imgNave.width / 2, canvas.height - imgNave.height / 20, 10);
+        nave.desenhar();
+
+        document.addEventListener("keydown", function(evento) {
+             tecla = evento.key;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            if (tecla === "ArrowLeft") {
+                nave.esquerda();
+            } else if (tecla === "ArrowRight") {
+                nave.direita();
+            } else if (tecla === "ArrowUp") {
+                nave.subir();
+            } else if (tecla === "ArrowDown") {
+                nave.descer();
+            }
+            nave.desenhar();
+        });
     }
-}
-this.subir=function(){
-    if(this.y>0){
-        this.y-=this.deslocamento;
-    }
-}
-this.descer=function(){
-    if(this.t<=this.contexto.canvas.height-this.altura-this.deslocamento){
-        this.y+=this.deslocamento;
-    }
-}
-}
+
